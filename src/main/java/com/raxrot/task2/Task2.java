10274e15c72351b11673ac33f9b22b76e9647c93
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-09-03 11:12:27 +0100

    Task 2 - Two Sums

---
+++ src/main/java/com/raxrot/task2/Task2.java
@@ -0,0 +1,17 @@
+package com.raxrot.task2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int []arr={1,2,5,6,7,8};
+        int totalSum=11;
+        for(int i=0;i<arr.length;i++){
+            for (int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==totalSum){
+                    System.out.println(arr[i]+","+arr[j]);
+                    return;
+                }
+            }
+        }
+        System.out.println("No Sum");
+    }
+}
